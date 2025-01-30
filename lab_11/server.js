dc95872ec74a4b1f081840e637d3be52b7815eb7
Jack Dunn <j.dunn4@rgu.ac.uk>
2025-01-30 10:54:57 +0000

    /add route code added

--- lab_11/server.js
+++ lab_11/server.js
@@ -9,4 +9,16 @@ app.get('/test', function(req, res){
     res.send("this is route 2");
 });
 
+app.get('/joke', function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var randomJoke = knockknock();
+    res.end(randomJoke);
+});
+
+app.get('/add', function(req, res){
+    var x = erq.query.x;
+    var y = req.query.y;
+    res.send("X + Y="+(x+y));
+});
+
 app.listen(8080);
