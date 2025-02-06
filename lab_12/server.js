98b1709199bf5c615ece25b526fe9be5eca5e7f5
Jack Dunn <j.dunn4@rgu.ac.uk>
2025-02-06 11:28:23 +0000

    fixed auth?

--- lab_12/server.js
+++ lab_12/server.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
 var SpotifyWebApi = require('spotify-web-api-node');
-app.use(express.static('public'))
-
 var spotifyApi = new SpotifyWebApi({
     clientID: '99891f61c9f045e78fdb6255e10b1ac7',
     clientSecret: 'fe12841c94364704b71f6b45f6a8e624'
 });
+app.use(express.static('public'))
+
 
 app.get('/', function(req, res){
     res.send("Hello world! by express");
