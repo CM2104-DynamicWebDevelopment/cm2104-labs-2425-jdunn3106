98718cbeb827f52767693d13a71c23f2e6439ec7
Jack Dunn <j.dunn4@rgu.ac.uk>
2025-02-06 11:01:52 +0000

    Auth for api added

--- lab_12/server.js
+++ lab_12/server.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var app = express();
+var SpotifyWebApi = require('spotify-web-api-node');
 app.use(express.static('public'))
+
+var spotifyApi = new SpotifyWebApi({
+    clientID: '99891f61c9f045e78fdb6255e10b1ac7',
+    clientSecret: 'fe12841c94364704b71f6b45f6a8e624'
+});
 
 app.get('/', function(req, res){
     res.send("Hello world! by express");
 });
+
+spotifyApi.clientCredentialsGrant().then(
+    function(data) {
+        console.log('The access token expires in ' + data.body['expires_in']);
+        console.log('The access token is ' + data.body['access_token']);
+
+        spotifyApi.setAccessToken(data.body['access_token']);
+    },
+    function(err) {
+        console.log(
+            'Something went wrong when retrieving an access token',
+            err.message
+        );
+    }
+);
 
 app.listen(8080);
